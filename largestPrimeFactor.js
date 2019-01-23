622df92fa7cec3771b0ec7b0484e5b7d8247677e
Tyler <alexithemia@gmail.com>
2019-01-23 12:21:53 -1000

    Commented code removed

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -2,26 +2,13 @@ exports.largestPrimeFactor = function (n) {
   var primeNumber = 2;
   while (n > 2) {
     if (n % primeNumber === 0) {
+      console.log(n + " before")
       n = n / primeNumber;
+      console.log(primeNumber + ' Prime')
+      console.log(n + " number after")
     } else (
       primeNumber++
     )
   }
-  // function isPrime(num) {
-  //   var i = 2;
-  //   while (i < num) {
-  //     if (num % i === 0) {
-  //       return false;
-  //     }
-  //     i++;
-  //   }
-  //   return true;
-  // }
-
-  // for (var i = 2; i < n; i++) {
-  //   if (n % i === 0 && isPrime(i)) {
-  //     primeNumber = i;
-  //   }
-  // }
   return primeNumber;
 };
